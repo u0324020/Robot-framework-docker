afc733a5c25fb68e789a27164a12a145d75d6fdd
jane-yc-chen <jane_yc_chen@trendmicro.com>
2021-09-27 10:18:32 +0800

    add while

--- test.sh
+++ test.sh
@@ -1,6 +1,22 @@
-# cd robot
-# docker-compose up -d
-# sleep 5s
-[ -f reports/log.html ] && docker-compose down && echo "[ Robot-Debug ] l1-testing is finished" || echo "[ Robot-Debug ] log file not exists && exit 1"
-echo "hi"
-cd -
+
+# while [ ! -f Dockerfile ]
+#   do
+#      sleep SOME_SECONDS
+#   done
+# echo "file found"
+
+# After 60 seconds the loop will exit
+timeout=300
+
+while [ ! -f Dockerfile ];
+do
+  # When the timeout is equal to zero, show an error and leave the loop.
+  if [ "$timeout" == 0 ]; then
+    echo "ERROR: Timeout while waiting for the file /tmp/list.txt."
+    exit 1
+  fi
+  sleep 1
+  ((timeout--))
+  echo "[ Robot-Debug ] waiting for reports generate: $timeout s"
+done
+echo "file found"
